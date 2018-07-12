6d1af70d1479d42990ad1f55917e1211e7b72b75
jupterorbita <john.williams@outlook.com>
2018-07-12 15:52:49 -0700

    init commit

---
+++ getFilmsSW_API/getFilmsSW_API/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  getFilmsSW_API
+//
+//  Created by J on 7/12/2018.
+//  Copyright © 2018 Jman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
